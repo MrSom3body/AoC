0a4ae46d1eda53e3dbb32284666ef06d28ac8943
Karun Sandhu <karun@sndh.dev>
2025-12-05 22:55:39 +0100

    2025/03: implement part 2

--- 2025/03/main.go
+++ 2025/03/main.go
@@ -35,8 +35,56 @@ func task1(lines []string) int {
 	return output_joltage
 }
 
+func sliceToInt(s []int) int {
+	res := 0
+	op := 1
+	for i := len(s) - 1; i >= 0; i-- {
+		res += s[i] * op
+		op *= 10
+	}
+	return res
+}
+
+func findBiggestNumbers(numbers []int) int {
+	var result []int
+
+	start := 0
+	itemsToPick := 12
+	total := len(numbers)
+
+	for i := range itemsToPick {
+		remaining := itemsToPick - 1 - i
+		end := (total - 1) - remaining
+
+		bestDigit := -1
+		bestIndex := -1
+
+		for j := start; j <= end; j++ {
+			val := numbers[j]
+			if val > bestDigit {
+				bestDigit = val
+				bestIndex = j
+			}
+		}
+
+		result = append(result, bestDigit)
+		start = bestIndex + 1
+	}
+
+	return sliceToInt(result)
+}
+
 func task2(lines []string) int {
-	return 0
+	joltage := 0
+	for _, line := range lines {
+		n := make([]int, len(line))
+		for i, ch := range line {
+			n[i], _ = strconv.Atoi(string(ch))
+		}
+		big := findBiggestNumbers(n)
+		joltage += big
+	}
+	return joltage
 }
 
 func main() {
@@ -48,11 +96,11 @@ func main() {
 	input = aoclib.ReadInput("./2025/03/i.in")
 	aoclib.Output(task1(input))
 
-	// // test input
-	// input = aoclib.ReadInput("./2025/03/x.in")
-	// aoclib.Output(task2(input))
+	// test input
+	input = aoclib.ReadInput("./2025/03/x.in")
+	aoclib.Output(task2(input))
 
-	// // normal input
-	// input = aoclib.ReadInput("./2025/03/i.in")
-	// aoclib.Output(task2(input))
+	// normal input
+	input = aoclib.ReadInput("./2025/03/i.in")
+	aoclib.Output(task2(input))
 }
